8b7e87842c2874bf6d2235ebcf0931645078beb3
Jaber Abbas <jaber.h.abbas@gmail.com>
2024-12-15 22:05:52 +0100

    return secrets in json format and deserialize them into object

--- src/main/java/com/example/secret_management/GetSecretValue.java
+++ src/main/java/com/example/secret_management/GetSecretValue.java
@@ -1,68 +1,45 @@
 package com.example.secret_management;
 
+import com.google.gson.Gson;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
 import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
 import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueResponse;
 import software.amazon.awssdk.services.secretsmanager.model.SecretsManagerException;
 
-/**
- * Before running this Java V2 code example, set up your development
- * environment, including your credentials.
- *
- * For more information, see the following documentation topic:
- *
- * https://docs.aws.amazon.com/sdk-for-java/latest/developer-guide/get-started.html
- *
- * We recommend that you cache your secret values by using client-side caching.
- *
- * Caching secrets improves speed and reduces your costs. For more information,
- * see the following documentation topic:
- *
- * https://docs.aws.amazon.com/secretsmanager/latest/userguide/retrieving-secrets.html
- */
 public class GetSecretValue {
     public static void main(String[] args) {
-        final String usage = """
-
-                Usage:
-                    <secretName>\s
-
-                Where:
-                    secretName - The name of the secret (for example, tutorials/MyFirstSecret).\s
-                """;
-
-/*
-        if (args.length != 1) {
-            System.out.println(usage);
-            System.exit(1);
-        }
-*/
-
         String secretName = "dev/sql/pass";
         Region region = Region.EU_WEST_3;
         SecretsManagerClient secretsClient = SecretsManagerClient.builder()
                 .region(region)
                 .build();
 
-        getValue(secretsClient, secretName);
+        Gson gson = new Gson();
+        String secret = getValue(secretsClient, secretName);
+        if (secret != null) {
+            User user = gson.fromJson(String.valueOf(secret), User.class);
+            System.out.println("object: " + user.getUsername() + " " + user.getPassword() + " " + user.getHost());
+        }
         secretsClient.close();
     }
 
-    public static void getValue(SecretsManagerClient secretsClient, String secretName) {
+    public static String getValue(SecretsManagerClient secretsClient, String secretName) {
+        String secret = null;
         try {
             GetSecretValueRequest valueRequest = GetSecretValueRequest.builder()
                     .secretId(secretName)
                     .build();
 
             GetSecretValueResponse valueResponse = secretsClient.getSecretValue(valueRequest);
-            String secret = valueResponse.secretString();
-            System.out.println(secret);
+            secret = valueResponse.secretString();
+            System.out.println("json format: " + secret);
 
         } catch (SecretsManagerException e) {
             System.err.println(e.awsErrorDetails().errorMessage());
             System.exit(1);
         }
+        return secret;
     }
 }
 
