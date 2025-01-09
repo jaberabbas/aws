1c5e17442ad102b21d702b80b3499bf5d07e3524
Jaber Abbas <jaber.h.abbas@gmail.com>
2025-01-09 22:31:05 +0100

    Java stores items in DyanamoDB in AWS and sends SMS to mobile

---
+++ src/main/java/com/example/GreetingsApplication.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetingsApplication.class, args);
+		System.out.println("Hello Amazon");
+	}
+
+}
