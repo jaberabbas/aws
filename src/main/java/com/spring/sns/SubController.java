9283b5f83acd5b7beba60b7cebf5bf4d4609ec48
Jaber Abbas <jaber.h.abbas@gmail.com>
2025-01-12 16:20:42 +0100

    subscribe/publish to SNS topic

---
+++ src/main/java/com/spring/sns/SubController.java
@@ -0,0 +1,62 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package com.spring.sns;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+
+@Controller
+public class SubController {
+    private final SnsService sns;
+
+    @Autowired
+    SubController(SnsService sns) {
+        this.sns = sns;
+    }
+
+    @GetMapping("/")
+    public String root() {
+        return "index";
+    }
+
+    @GetMapping("/subscribe")
+    public String add() {
+        return "sub";
+    }
+
+    @RequestMapping(value = "/addEmail", method = RequestMethod.POST)
+    @ResponseBody
+    String addItems(HttpServletRequest request, HttpServletResponse response) {
+        String email = request.getParameter("email");
+        return sns.subEmail(email);
+    }
+
+    @RequestMapping(value = "/delSub", method = RequestMethod.POST)
+    @ResponseBody
+    String delSub(HttpServletRequest request, HttpServletResponse response) {
+        String email = request.getParameter("email");
+        sns.unSubEmail(email);
+        return email + " was successfully deleted!";
+    }
+
+    @RequestMapping(value = "/addMessage", method = RequestMethod.POST)
+    @ResponseBody
+    String addMessage(HttpServletRequest request, HttpServletResponse response) {
+
+        String body = request.getParameter("body");
+        String lang = request.getParameter("lang");
+        return sns.pubTopic(body, lang);
+    }
+
+    @RequestMapping(value = "/getSubs", method = RequestMethod.GET)
+    @ResponseBody
+    String getSubs(HttpServletRequest request, HttpServletResponse response) {
+        return sns.getAllSubscriptions();
+    }
+}
